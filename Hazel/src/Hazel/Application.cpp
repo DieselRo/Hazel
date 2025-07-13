06a06fbc108ec6959c522f78f9f491b00384ab20
DieselRo <Riedeselluker@gmail.com>
2025-07-12 22:17:58 -0500

    Added Window class

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -2,15 +2,18 @@
 
 #include "hzpch.h"
 #include "Application.h"
+
 #include "Hazel/Events/ApplicationEvent.h"
 #include "Hazel/Log.h"
+
+#include <GLFW/glfw3.h>
 
 namespace Hazel {
 
 	Application::Application()
 	{
+		m_Window = std::unique_ptr<Window>(Window::Create());
 	}
-
 
 	Application::~Application()
 	{
@@ -18,17 +21,12 @@ namespace Hazel {
 
 	void Application::Run()
 	{
-		WindowResizeEvent e(1280, 720);
-		if (e.IsInCategory(EventCategoryApplication))
+		while (m_Running)
 		{
-			::Hazel::Log::GetClientLogger()->trace("{0}", e);
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
 		}
-		if (e.IsInCategory(EventCategoryInput))
-		{
-			::Hazel::Log::GetClientLogger()->trace("{0}", e);
-		}
-
-		while (true);
 	}
 
 }
