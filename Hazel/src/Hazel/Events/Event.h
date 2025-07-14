493ddb6849b9c1b781da28f83b87e8fc05f46121
DieselRo <Riedeselluker@gmail.com>
2025-07-14 07:19:59 -0500

    Added Layerstack

--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -48,8 +48,8 @@ namespace Hazel {
 
 	class HAZEL_API Event
 	{
-		friend class EventDispatcher;
 	public:
+		bool Handled = false;
 		virtual EventType GetEventType() const = 0;
 		virtual const char* GetName() const = 0;
 		virtual int GetCategoryFlags() const = 0;
@@ -59,8 +59,6 @@ namespace Hazel {
 		{
 			return GetCategoryFlags() & category;
 		}
-	protected:
-		bool m_Handled = false;
 	};
 	
 
@@ -79,7 +77,7 @@ namespace Hazel {
 		{
 			if (m_Event.GetEventType() == T::GetStaticType())
 			{
-				m_Event.m_Handled = func(*(T*)&m_Event);
+				m_Event.Handled = func(*(T*)&m_Event);
 				return true;
 			}
 			return false;
---
+++ Hazel/src/Hazel/Layer.cpp
@@ -0,0 +1,17 @@
+//Layer.cpp
+#include "hzpch.h"
+#include "Layer.h"
+
+namespace Hazel {
+
+	Layer::Layer(const std::string& debugName)
+		: m_DebugName(debugName)
+	{
+	}
+
+	Layer::~Layer()
+	{
+
+	}
+
+}
